f186ab943f688c7874ecb76e0f541f97a29f0c87
MariamAziz0 <ma27523@gmail.com>
2024-11-29 21:40:40 +0200

    Transition table container has been implemented and tested

---
+++ src/TransitionTableContainer.cpp
@@ -0,0 +1,112 @@
+#include "TransitionTableContainer.h"
+
+void TransitionTableContainer::createTransitionTable(
+        unordered_map<state, unordered_map<symbol, state>> transMap,
+        unordered_map<state, clazz> acceptingStates,
+        state initialState,
+        unordered_set<symbol> symbols
+        ) {
+    this->acceptingStates = acceptingStates;
+    this->initialState = initialState;
+
+    int index = 0;
+    for(auto& symbol: symbols) {
+        this->symbolToIndexMapper[symbol] = index++;
+    }
+
+    for(auto& outerPair: transMap) {
+        state currentState = outerPair.first;
+
+        for(auto& transition: outerPair.second) {
+            int currentSymbolIndex = this->symbolToIndexMapper[transition.first];
+            this->transitionTable[currentState][currentSymbolIndex] = transition.second;
+        }
+    }
+}
+
+bool TransitionTableContainer::writeFile(string &filePath) {
+    ofstream outFile(filePath);
+
+    if (!outFile) {
+        cerr << "Error opening file for writing the transition table!" << endl;
+        return false;
+    }
+
+    int rows = transitionTable.size();
+    int cols = (rows > 0) ? transitionTable[0].size() : 0;
+
+    outFile << rows << " " << cols << endl;
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            outFile << transitionTable[i][j] << " ";
+        }
+        outFile << endl;
+    }
+
+    for (const auto& symbolIndexPair : symbolToIndexMapper) {
+        outFile << symbolIndexPair.first << " " << symbolIndexPair.second << endl;
+    }
+    outFile << endl;
+
+    for (const auto& acceptingState : acceptingStates) {
+        outFile << acceptingState.first << " " << acceptingState.second << endl;
+    }
+
+    outFile << endl << initialState << endl;
+
+    outFile.close();
+    return true;
+}
+
+bool TransitionTableContainer::readFile(string &filePath) {
+    ifstream inFile(filePath);
+
+    if (!inFile) {
+        cerr << "Error opening transition table file for reading!" << endl;
+        return false;
+    }
+
+    string line;
+    int rows, cols;
+
+    getline(inFile, line);
+    stringstream ss(line);
+    ss >> rows >> cols;
+
+    transitionTable.resize(rows, vector<int>(cols));
+
+    for (int i = 0; i < rows; i++) {
+        getline(inFile, line);
+        stringstream rowStream(line);
+        for (int j = 0; j < cols; j++) {
+            rowStream >> transitionTable[i][j];
+        }
+    }
+
+    while (getline(inFile, line)) {
+        if (line.empty()) break;
+        stringstream symbolStream(line);
+        char symbol;
+        int index;
+        symbolStream >> symbol >> index;
+        symbolToIndexMapper[symbol] = index;
+    }
+
+    while (getline(inFile, line)) {
+        if (line.empty()) break;
+        stringstream acceptingStream(line);
+        int stateId;
+        string stateName;
+        acceptingStream >> stateId;
+        getline(acceptingStream, stateName);
+        acceptingStates[stateId] = stateName.substr(1);
+    }
+
+    getline(inFile, line);
+    stringstream initialStateStream(line);
+    initialStateStream >> initialState;
+
+    inFile.close();
+    return true;
+}
