521ee78afbcbc359df2ff83049bf2b9cbbaeedd4
MariamAziz0 <ma27523@gmail.com>
2024-12-04 12:19:38 +0200

    Resolve comments

--- src/lexical-analyzer/LexicalAnalyzer.cpp
+++ src/lexical-analyzer/LexicalAnalyzer.cpp
@@ -27,24 +27,20 @@ pair<clazz, lexem> LexicalAnalyzer::getNextToken() {
     char currentCharacter;
     state currentState = this->initialState;
     state nextState;
-    vector<int> currentTokenStates;
+    vector<state> currentTokenStates;
 
     // Check if there are some remaining characters from the last time
     for(int i = 0; i < this->remainingCharacters.size(); i++) {
         currentCharacter = this->remainingCharacters[i];
         token += currentCharacter;
 
-        if(this->symbolToIndexMapper.count(currentCharacter) != 0) {
-            nextState = this->transitionTable[currentState][this->symbolToIndexMapper[currentCharacter]];
-            currentTokenStates.push_back(nextState);
+        nextState = (this->symbolToIndexMapper.count(currentCharacter) != 0)?
+                    this->transitionTable[currentState][this->symbolToIndexMapper[currentCharacter]] :
+                    REJECTING_STATE;
 
-            if(nextState == REJECTING_STATE) {
-                this->remainingCharacters = this->remainingCharacters.substr(token.size());
-                return checkCurrentState(token, currentTokenStates);
-            }
-        }
-        else {
-            currentTokenStates.push_back(REJECTING_STATE);
+        currentTokenStates.push_back(nextState);
+
+        if(nextState == REJECTING_STATE) {
             this->remainingCharacters = this->remainingCharacters.substr(token.size());
             return checkCurrentState(token, currentTokenStates);
         }
@@ -62,24 +58,19 @@ pair<clazz, lexem> LexicalAnalyzer::getNextToken() {
             }
         }
 
-        if(this->symbolToIndexMapper.count(currentCharacter) != 0) {
-            nextState = this->transitionTable[currentState][this->symbolToIndexMapper[currentCharacter]];
+        token += currentCharacter;
 
-            token += currentCharacter;
-            currentTokenStates.push_back(nextState);
+        nextState = (this->symbolToIndexMapper.count(currentCharacter) != 0) ?
+                    this->transitionTable[currentState][this->symbolToIndexMapper[currentCharacter]] :
+                    REJECTING_STATE;
 
-            if (nextState == REJECTING_STATE) {
-                return checkCurrentState(token, currentTokenStates);
-            }
+        currentTokenStates.push_back(nextState);
 
-            currentState = nextState;
-        }
-        else {
-            token += currentCharacter;
-            currentTokenStates.push_back(REJECTING_STATE);
-
+        if (nextState == REJECTING_STATE) {
             return checkCurrentState(token, currentTokenStates);
         }
+
+        currentState = nextState;
     }
 
     return checkCurrentState(token, currentTokenStates);
@@ -90,28 +81,23 @@ void LexicalAnalyzer::printErrorMessage(lexem token) {
 }
 
 bool LexicalAnalyzer::isNextTokenAvailable() {
-    return this->inputFilePointer.good();
+    return this->inputFilePointer.good() || !this->remainingCharacters.empty();
 }
 
 pair<clazz, lexem> LexicalAnalyzer::checkCurrentState(lexem token, vector<int> currentTokenStates) {
     if(currentTokenStates.empty()) {
         return {};
     }
-    state lastState = currentTokenStates[currentTokenStates.size() - 1];
-    if (this->acceptingStates.count(lastState) != 0) {
-        return {this->acceptingStates[lastState], token};
-    }
-    else {
-        for(int i = currentTokenStates.size() - 1; i >= 0; i--) {
-            if(this->acceptingStates.count(currentTokenStates[i]) != 0) {
-                lexem tmpToken = token.substr(0, i + 1);
-                this->remainingCharacters = token.substr(i + 1) + this->remainingCharacters;
 
-                return {this->acceptingStates[currentTokenStates[i]], tmpToken};
-            }
+    for(int i = currentTokenStates.size() - 1; i >= 0; i--) {
+        if(this->acceptingStates.count(currentTokenStates[i]) != 0) {
+            lexem tmpToken = token.substr(0, i + 1);
+            this->remainingCharacters = token.substr(i + 1) + this->remainingCharacters;
+
+            return {this->acceptingStates[currentTokenStates[i]], tmpToken};
         }
-
-        printErrorMessage(token);
-        return {ERROR_MESSAGE, token};
     }
+
+    printErrorMessage(token);
+    return {ERROR_MESSAGE, token};
 }
